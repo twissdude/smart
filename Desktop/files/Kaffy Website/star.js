191ebbb6f33f54de13a8007402732d40d61f3724
Tajudeen Woye <woyetajudeen@gmail.com>
2020-10-19 05:51:27 +0100

    Shop Smart

---
+++ Desktop/files/Kaffy Website/star.js
@@ -0,0 +1,18 @@
+let navbar = document.querySelector(".navbar");
+let ham = document.querySelector(".ham");
+ham.addEventListener("click", toggleHamburger);
+function toggleHamburger() {
+    navbar.classList.toggle("showNav");
+    ham.classList.toggle("showClose");
+}
+
+let app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.count = 0;
+
+});
+
+
+alert("Please call us on 08078698593");
+
+
