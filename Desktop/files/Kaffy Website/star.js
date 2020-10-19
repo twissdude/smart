e00f063c2368bc66a2a146a1e37856dc656b57ab
Tajudeen Woye <woyetajudeen@gmail.com>
2020-10-19 11:44:18 +0100

    smart

--- Desktop/files/Kaffy Website/star.js
+++ Desktop/files/Kaffy Website/star.js
@@ -1,10 +1,17 @@
-let navbar = document.querySelector(".navbar");
-let ham = document.querySelector(".ham");
-ham.addEventListener("click", toggleHamburger);
+let navbar = document.querySelector(".navbar")
+let ham = document.querySelector(".ham")
+ham.addEventListener("click", toggleHamburger)
 function toggleHamburger() {
-    navbar.classList.toggle("showNav");
-    ham.classList.toggle("showClose");
+    navbar.classList.toggle(".showNav")
+    ham.classList.toggle(".showClose")
 }
+
+let menuLinks = document.querySelectorAll(".menuLink")
+menuLinks.forEach(
+    function (menuLink) {
+        menuLink.addEventListener("click", toggleHamburger)
+    }
+)
 
 let app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope) {
